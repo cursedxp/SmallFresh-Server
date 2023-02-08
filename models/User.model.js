36038be72e8cd780174d8a17ca607091a033e732
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-08 21:39:12 +0100

    orders model ref has been added to users model

--- models/User.model.js
+++ models/User.model.js
@@ -28,8 +28,9 @@ const userSchema = new Schema(
       type: String,
       required: [true, "Password is required."],
     },
-    addresses: [{ type: mongoose.Types.ObjectId, ref: "Address" }],
-    favProducts: [{ type: mongoose.Types.ObjectId, ref: "Product" }],
+    addresses: [{ type: mongoose.Schema.Types.ObjectId, ref: "Address" }],
+    favProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
