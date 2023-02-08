a78d44bd100713bed16de7f2f2fa891fd4fff191
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-08 19:07:08 +0100

    Creating address model

--- models/Address.model.js
+++ models/Address.model.js
@@ -1,30 +1,35 @@
 const { Schema, model } = require("mongoose");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const addressSchema = new Schema({
-    addressType:{
-        type:String,
-        enum:['Home','Office','Other'],
-        default:'Home',
-    },
-    street: {
-        type: String,
-        required: true
-      },
-    city: {
-        type: String,
-        required: true
-      },
-    state: {
-        type: String,
-        required: true
-      },
-    zip: {
-        type: String,
-        required: true
-      },
-    user:{type:mongoose.Types.ObjectId,ref:'User'},
-})
+  addressType: {
+    type: String,
+    enum: ["Home", "Office", "Other"],
+    default: "Home",
+  },
+  street: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: String,
+    required: true,
+  },
+  description: { type: String },
+  coordinates: {
+    latitude: { type: Number },
+    longitude: { type: Number },
+  },
+  user: { type: mongoose.Types.ObjectId, ref: "User" },
+});
 
 const User = model("Address", addressSchema);
 
