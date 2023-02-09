9cae41ccacf0ca1ca48dda47b96185cd371ef78d
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-09 19:56:17 +0100

    product routes has been created

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,63 @@
 const express = require("express");
 const router = express.Router();
 const productSeed = require("../seed/products.json");
+
+//Models
 const Product = require("../models/Product.model");
 
-router.get("/seed", (req, res) => {
-  res.json("Hello");
-  Product.create(productSeed)
+// router.get("/seed", (req, res) => {
+//   res.json("Hello");
+//   Product.create(productSeed)
+//     .then((result) => {
+//       console.log("Collection has been seeded");
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
+
+//Products
+router.get("/products", (req, res, next) => {
+  Product.find()
+    .then((products) => {
+      res.json(products);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/products", (req, res, next) => {
+  const {
+    name,
+    img,
+    category,
+    description,
+    bio,
+    piece,
+    unit,
+    brand,
+    amount,
+    price,
+  } = req.body;
+
+  Product.create({
+    name: name,
+    img: img,
+    category: category,
+    description: category,
+    bio: bio,
+    stock: {
+      piece: piece,
+      unit: unit,
+      brand: brand,
+      amount: amount,
+      price: price,
+    },
+  })
     .then((result) => {
-      console.log("Collection has been seeded");
+      res.json(result);
+      console.log("New product has been added");
     })
     .catch((err) => {
       console.log(err);
