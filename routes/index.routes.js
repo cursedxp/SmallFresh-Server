fa7f10b6c712ae16f163e9d4b4362bb0956760bf
Anil Ozsoy <anilozsoy@gmail.com>
2023-02-09 14:12:21 +0100

    seed route for products

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const productSeed = require("../seed/products.json");
+const Product = require("../models/Product.model");
+
+router.get("/seed", (req, res) => {
+  res.json("Hello");
+  Product.create(productSeed)
+    .then((result) => {
+      console.log("Collection has been seeded");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
